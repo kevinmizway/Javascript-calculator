846e70dd29b39736533f46f105189d4e6e115e41
Kevin Mizway <mizzledragon@gmail.com>
2016-11-15 20:45:28 -0500

    Create user interface logic for addition, subtraction, multiplication, and division

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -14,7 +16,38 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+//Front End Logic
+
+$(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+
+    $("#output").text(add(number1,number2));
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+
+    $("#output").text(subtract(number1,number2));
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+
+    $("#output").text(multiply(number1,number2));
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+
+    $("#output").text(divide(number1,number2));
+  });
+});
