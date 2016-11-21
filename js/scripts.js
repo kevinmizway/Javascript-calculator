b7cf40d551517800e9a16c10459bf507a8702b80
Kevin Mizway <mizzledragon@gmail.com>
2016-11-20 19:52:46 -0500

    Updated html to include all operator functions as radio buttons. updated jquery with branching linked to functions.

--- js/scripts.js
+++ js/scripts.js
@@ -18,36 +18,22 @@ var divide = function(number1, number2) {
 
 //Front End Logic
 
-$(function() {
-  $("form#add").submit(function(event) {
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-
-    $("#output").text(add(number1,number2));
-  });
-
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-
-    $("#output").text(subtract(number1,number2));
-  });
-
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-
-    $("#output").text(multiply(number1,number2));
-  });
-
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-
-    $("#output").text(divide(number1,number2));
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
 });
